f2a474417c0174bbcb82b288d975a3430c95dc47
RichardFuentesTorreLinux <javierbaron6@gmail.com>
2025-06-24 14:29:09 -0500

    ajustes

--- angular/src/app/module/lista-componentes.ts
+++ angular/src/app/module/lista-componentes.ts
@@ -3,6 +3,10 @@ import { EditarUsuariosComponent } from "./users/components/editar-usuarios/edit
 import { VerUsuariosComponent } from "./users/components/ver-usuarios/ver-usuarios.component";
 import { FiltroUsuariosComponent } from './users/components/filtro/filtro.component';
 
+import { CrearModuloPermisoComponent } from './modules/components/crear-modulo-permiso/crear-modulo-permiso.component';
+import { EditarModuloPermisoComponent } from './modules/components/editar-modulo-permiso/editar-modulo-permiso.component';
+import { VerModuloPermisoComponent } from './modules/components/ver-modulo-permiso/ver-modulo-permiso.component';
+
 export class ListaComponentes {
 
   /*
@@ -24,7 +28,21 @@ export class ListaComponentes {
     },
     {
       name: 'FiltroUsuariosComponent',
-      componente: FiltroUsuariosComponent
+      componente: FiltroUsuariosComponent      
+    },
+    
+    // modulos permisos
+    {
+      name: 'CrearModuloPermisoComponent',
+      componente: CrearModuloPermisoComponent  
+    },
+    {
+      name: 'EditarModuloPermisoComponent',
+      componente: EditarModuloPermisoComponent  
+    },
+    {
+      name: 'VerModuloPermisoComponent',
+      componente: VerModuloPermisoComponent  
     },
 
   ];
---
+++ angular/src/app/module/modules/components/editar-modulo-permiso/editar-modulo-permiso.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-modulo-permiso',
+  templateUrl: './editar-modulo-permiso.component.html',
+  styleUrl: './editar-modulo-permiso.component.scss'
+})
+export class EditarModuloPermisoComponent {
+
+}
