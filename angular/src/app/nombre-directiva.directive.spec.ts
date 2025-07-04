65354fdf0a461cf03285aa449d8809435e338440
RichardFuentes369 <javierbaron6@gmail.com>
2025-07-03 23:36:42 -0500

    ajustes

---
+++ angular/src/app/nombre-directiva.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NombreDirectivaDirective } from './nombre-directiva.directive';
+
+describe('NombreDirectivaDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NombreDirectivaDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ angular/src/app/nombre-directiva.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appNombreDirectiva]',
+  standalone: false
+})
+export class NombreDirectivaDirective {
+
+  constructor() { }
+
+}
