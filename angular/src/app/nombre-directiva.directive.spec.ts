0b822bd0e97acfe5081d7e28ef328f0a14349b0e
RichardFuentesTorreLinux <javierbaron6@gmail.com>
2025-07-08 15:49:16 -0500

    ajustes

--- angular/src/app/nombre-directiva.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NombreDirectivaDirective } from './nombre-directiva.directive';
-
-describe('NombreDirectivaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NombreDirectivaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
