16079e36461fb1c20a2e33b545d63e8a78cc43f3
JavierHouseTower <javierbaron6@gmail.com>
2026-01-31 18:34:05 -0500

    ajustes

--- angular/src/app/layouts/home/home.component.ts
+++ angular/src/app/layouts/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, Renderer2, HostListener, OnInit, ViewChild } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core'
 import { IdiomaComponent } from '@component/globales/idioma/idioma.component';
-import { RouterModule } from '@angular/router';
+import { Router, NavigationEnd, Event, RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 import { NAME_PAGE, LAYOUT_HOME_PAGE_START, LAYOUT_HOME_PAGE_LOGIN_FINAL } from '@layout/const/layouts.const';
 import { ColormodeComponent } from '@component/globales/colormode/colormode.component';
@@ -18,20 +19,33 @@ import { ColormodeComponent } from '@component/globales/colormode/colormode.comp
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   public CURRENT_YEAR = new Date().getFullYear();
   public NAME_PAGE = NAME_PAGE;
   public LAYOUT_HOME_PAGE_START = LAYOUT_HOME_PAGE_START;
   public LAYOUT_HOME_PAGE_LOGIN_FINAL = LAYOUT_HOME_PAGE_LOGIN_FINAL;
 
-  constructor(private renderer: Renderer2, private translate: TranslateService,) {}
+  showRouteIngreso: boolean = true
+
+  constructor(private translate: TranslateService,private router: Router) {
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event:any) {
   }
 
-  ngOnInit(event:any) {
+  ngOnInit() {
+    this.verificarRuta(this.router.url);
+    this.router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.verificarRuta(event.urlAfterRedirects);
+    });
+  }
+
+  private verificarRuta(url: string) {
+    this.showRouteIngreso = !url.includes('ingreso/admin');
   }
 
   minimizarSliderbar: boolean = false;
