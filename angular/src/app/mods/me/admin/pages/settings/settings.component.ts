ac995cf8b8df41e8f5591178c6237a6cf3e10bc5
RichardFuentes369 <javierbaron6@gmail.com>
2026-02-01 17:15:12 -0500

    ajustes

--- angular/src/app/mods/me/admin/pages/settings/settings.component.ts
+++ angular/src/app/mods/me/admin/pages/settings/settings.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AvatarComponent } from '@component/globales/avatar/avatar.component';
+import { DropzoneComponent } from '@component/globales/dropzone/dropzone.component';
 
 @Component({
   selector: 'app-mod-me-admin-pages-settings',
   standalone: true,
+  imports: [AvatarComponent, DropzoneComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
